49f1a8c184d8481b6e4daba014cd355f5355fde5
LyqSpace <root@lyq.me>
2021-08-27 14:43:13 -0500

    update

--- eval.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python3 eval.py \
-    --budget 2500000 \
-    --resume logs/level0_20210201-012820/model/final.pth \
-    --viz \
-    --prefix fulltrain \
-    --video LSU-20200423 \
